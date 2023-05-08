dcb9f904be08e0c162f7f1eeb0aa851ca8cf7207
yousef <youssefnaro@gmail.com>
2023-05-08 21:49:00 +0100

    navbar update

--- client/src/component/navbar.jsx
+++ client/src/component/navbar.jsx
@@ -1,11 +1,15 @@
 import { HiMenuAlt4 } from 'react-icons/hi';
- import { AiOutlineClose } from 'react-icons/ai';
-function Navbar() {
+import { AiOutlineClose  } from 'react-icons/ai';
+
+ import logo from '../../images/logo.png';
+const Navbar = () => {
     return (
-        <div>
-        <HiMenuAlt4 />
-        <span>Cliquez pour ouvrir le menu</span>
-      </div>
+        <nav className="w-full flex md:justify-center justify-between items-center p-4 	text-decoration-color: #fff;">
+            <div className="md:flex-[0.5] flex-initial justify-center items-center">
+              <img src={logo} alt="logo" />
+            </div>
+           
+        </nav>
     );
 }
 export default Navbar;
