d7872563d3b1e1b9bb4221699055fecd5148e3a4
yousef <youssefnaro@gmail.com>
2023-05-20 17:20:54 +0100

    navbar Update

--- client/src/component/navbar.jsx
+++ client/src/component/navbar.jsx
@@ -4,11 +4,14 @@ import { AiOutlineClose  } from 'react-icons/ai';
  import logo from '../../images/logo.png';
 const Navbar = () => {
     return (
+        
         <nav className="w-full flex md:justify-center justify-between items-center p-4 	text-decoration-color: #fff;">
             <div className="md:flex-[0.5] flex-initial justify-center items-center">
               <img src={logo} alt="logo" />
             </div>
-           
+            {/* <div className ="w-full md:flex-[0.3] justify-between items-center ">
+                <input type="text" name="research" className='flex-initial justify-middle ' />
+            </div> */}
         </nav>
     );
 }
