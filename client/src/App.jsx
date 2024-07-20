e4c8590ea7f3df9d758e82b5f5a0f7b46444588f
yousef <youssefnaro@gmail.com>
2024-07-20 20:22:27 +0200

    Update App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,26 @@
+import React from 'react';
 import { Navbar, Welcome, Footer, Service, Transaction } from './component';
+import { shortenAddress } from './utilities';
+
 const App = () => {
+  // Supposons que vous ayez également d'autres props comme `connectWallet` et `currentAccount`
+  const connectWallet = () => { /* votre logique pour connecter le wallet */ };
+  const currentAccount = "0x1234567890abcdef1234567890abcdef12345678"; // Exemple d'adresse
+
   return (
     <div className='App'>
       <Navbar />
+      <Welcome
+        connectWallet={connectWallet}
+        currentAccount={currentAccount}
+        shortenAddress={shortenAddress}
+        // autres props nécessaires
+      />
       <Service />
       <Transaction />
-      <Welcome />
       <Footer />
     </div>
-);
+  );
 }
+
 export default App;
